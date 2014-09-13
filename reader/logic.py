9db51f772637385e708f85547034f30bc41bcebe
arya <aboudaie@brandeis.edu>
2014-09-13 10:50:31 -0400

    Added ABBYY stuff (I'll get it fixed) (Also added some logic stuff)

---
+++ reader/logic.py
@@ -0,0 +1,14 @@
+__author__ = 'arya'
+class Test:
+    def __init__(self,value=None,units=None):
+        self.name = None
+        self.brief_description = None
+        self.description = None
+        self.low = None
+        self.avg = None
+        self.med = None
+        self.meh_message = None
+        self.high = None
+        self.bad_message = None
+        self.value = value
+        self.units = units
--- reader/views.py
+++ reader/views.py
@@ -7,14 +7,17 @@ from .forms import UploadFileForm
 import Image
 import pytesseract
 import os
+from logic import Test
 from time import sleep
+from ABBYY import process
 
 # Imaginary function to handle an uploaded file.
 #from somewhere import handle_uploaded_file
 
 def handle_file(f):
     i = ImageFile(f)
-    print i
+    print i.name
+    print os.getcwd()
     image = Image.open(i)
     print image
     s = pytesseract.image_to_string(image)
@@ -27,9 +30,12 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_file(request.FILES['file'])
-            #i = Image.open('record.tif')
-            #pytesseract.image_to_string(i)
+            s = handle_file(request.FILES['file']) #Assume this function takes the image and gets text from it
+
+            #Create a list of Tests and generate them
+            #Call a function to find the values of those tests, if found add the file to the list
+
+
             return HttpResponseRedirect('/results')
     else:
         form = UploadFileForm()
